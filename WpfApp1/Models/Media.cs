35fc0d0c9a900c5ef0a4ffba181205ed53039a51
21120502 <37113714+Flashback054@users.noreply.github.com>
2024-01-06 23:02:24 +0700

    Media model

--- WpfApp1/Models/Media.cs
+++ WpfApp1/Models/Media.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MediaPlayer.Models
 {
-    internal class Media
+    public class Media : INotifyPropertyChanged
     {
+        public string? Name { get; set; }
+        public string? Uri { get; set; }
+        public TimeSpan LastSeekPosition { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
