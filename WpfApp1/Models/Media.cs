8aad3ae7ffeb6874d0bd1f6da6c56644eee62ccf
21120502 <37113714+Flashback054@users.noreply.github.com>
2024-01-07 17:14:03 +0700

    fix: Change Media model class

--- WpfApp1/Models/Media.cs
+++ WpfApp1/Models/Media.cs
@@ -7,20 +7,12 @@ using System.Threading.Tasks;
 
 namespace MediaPlayer.Models
 {
-    public class Media : INotifyPropertyChanged
+    public class Media(string? name, Uri? uri, string? thumbnail, TimeSpan? lastSeekPosition) : INotifyPropertyChanged
     {
-        public string? Name { get; set; }
-        public Uri? Uri { get; set; }
-        public string? Thumbnail { get; set; }
-        public TimeSpan LastSeekPosition { get; set; }
-
-        public Media(string? name, Uri? uri, string? thumbnail, TimeSpan? lastSeekPosition)
-        {
-            Name = name;
-            Uri = uri;
-            Thumbnail = thumbnail ?? "/Assets/Icons/default.png";
-            LastSeekPosition = lastSeekPosition ?? TimeSpan.Zero;
-        }
+        public string? Name { get; set; } = name;
+        public Uri? Uri { get; set; } = uri;
+        public string? Thumbnail { get; set; } = thumbnail ?? "/Assets/Icons/default.png";
+        public TimeSpan LastSeekPosition { get; set; } = lastSeekPosition ?? TimeSpan.Zero;
 
         public event PropertyChangedEventHandler? PropertyChanged;
     }
