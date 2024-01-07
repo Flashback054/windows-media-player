574a9170317bd96aada97b460a72cc1cdf0c940f
21120521 <nguyenphucphat111999@gmail.com>
2024-01-07 15:50:36 +0700

    add: Playlist.cs AddPlaylist window, Edit: MainWindow

--- WpfApp1/Models/Playlist.cs
+++ WpfApp1/Models/Playlist.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MediaPlayer.Models
 {
-    internal class Playlist
+    public class Playlist : INotifyPropertyChanged
     {
+        public string PlaylistName;
+
+        private ObservableCollection<Media> MediaList;
+
+        public Playlist()
+        {
+            PlaylistName = "";
+            MediaList = new ObservableCollection<Media>();
+        }
+
+        public void AddMediaFile(Media media)
+        {
+            MediaList.Add(media);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
