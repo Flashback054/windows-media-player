b4051a0af4c115929418f11e7208e3a53ea778e5
21120521 <nguyenphucphat111999@gmail.com>
2024-01-11 22:52:52 +0700

    Add PlaylistDoubleClick

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -174,7 +174,7 @@ namespace MediaPlayer
                                 //Close the file
                                 sw.Close();
 
-                                Media media = new Media(Path.GetFileName(filename), new Uri(filename));
+                                Media media = new Media(Path.GetFileName(filename), new Uri(filename),null,null);
                                 selectedPlaylist.AddMediaFile(media);
                                 selectedPlaylist.CountMedia = selectedPlaylist.CountPlaylistItems();
 
@@ -230,7 +230,7 @@ namespace MediaPlayer
                         foreach (string line in lines)
                         {
                             
-                            Media media = new Media(Path.GetFileName(line), new Uri(line));
+                            Media media = new Media(Path.GetFileName(line), new Uri(line), null, null);
 
                             playlist.AddMediaFile(media);
                         }
@@ -292,7 +292,7 @@ namespace MediaPlayer
                         string line = lines[i];
                         if (File.Exists(line))
                         {
-                            Media media = new Media(Path.GetFileNameWithoutExtension(line), new Uri(line));
+                            Media media = new Media(Path.GetFileNameWithoutExtension(line), new Uri(line), null, null);
                             RecentFiles.Add(media);
                         }
                     }
@@ -359,7 +359,7 @@ namespace MediaPlayer
 
             for (int j = 0; j < totalMedia; j++)
             {
-                Media media = new Media(currentPlaylist.MediaList[j].Name, currentPlaylist.MediaList[j].Uri);
+                Media media = new Media(currentPlaylist.MediaList[j].Name, currentPlaylist.MediaList[j].Uri,null,null);
 
                 SelectedPlaylist.Add(media);
             }
@@ -368,26 +368,47 @@ namespace MediaPlayer
 
         }
 
-        private void MediaFilesDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void PlaylistDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var media = Media_Files.SelectedItem as Media;
 
-            if (Media_Files.SelectedIndex < 0)
+            int i = Playlist.SelectedIndex;
+
+            currentPlaylistIndex = i;
+
+            var selectedPlaylist = AllPlaylist[currentPlaylistIndex];
+
+            if (selectedPlaylist.MediaList.Count == 0)
                 return;
 
+            var mediaIndex = 0;
+
+
+            //----------open media player window----------------
             //var screen = new Media_Playing(Media_Files.SelectedIndex);
+
+
+       
+        }
+
+        private void MediaFilesDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var mediaIndex = Media_Files.SelectedIndex;
+
+            var media = SelectedPlaylist[mediaIndex];
+
+            var selectedPlaylist = AllPlaylist[currentPlaylistIndex];
+
+
+            if (mediaIndex < 0)
+                return;
+
+            //----------open media player window----------------
+            //var screen = new Media_Playing(Media_Files.SelectedIndex);
+
      
             string pathString = media.Uri.LocalPath;
 
             AddToRecentMedia(pathString);
-            //if (screen.ShowDialog() == true)
-            //{
-
-            //}
-            //else
-            //{
-
-            //}
 
             ReadRecentFiles();
         }
