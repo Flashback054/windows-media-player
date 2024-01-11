4e74d0b7a7921573e12695c1fa966d6e3fb15ebc
21120502 <37113714+Flashback054@users.noreply.github.com>
2024-01-11 19:00:38 +0700

    feat: Finish all required functionalities

---
+++ WpfApp1/Utils/RelativeToAbsoluteConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MediaPlayer
+{
+    public class RelativeToAbsolutePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var relativePath = (string)value;
+            var folder = AppDomain.CurrentDomain.BaseDirectory;
+            var absolutePath = $"{folder}{relativePath}";
+
+            return absolutePath;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
